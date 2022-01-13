df8c36c2810908bb93f1ee23f513222eb918dd49
Igor <igor_motyka@mail.com>
2022-01-13 14:42:11 +0100

    Playing with concurrency

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,55 @@
+use std::{
+    collections::VecDeque,
+    fs::File,
+    io::Read,
+    ops::Range,
+    sync::{Arc, Mutex},
+    thread,
+    time::Duration,
+};
+
+trait Document<'a>: Iterator {
+    fn change(range: &Range<usize>, with: &str);
+}
+
+struct ConcurrentDocument {
+    queque: VecDeque<String>,
+}
+impl ConcurrentDocument {
+    pub fn new() -> Self {
+        Self {
+            queque: VecDeque::new(),
+        }
+    }
+    pub fn add(&mut self, text: String) {
+        self.queque.push_back(text);
+    }
+    pub fn iter(&self) -> std::collections::vec_deque::Iter<String> {
+        self.queque.iter()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let arc = Arc::new(Mutex::from(ConcurrentDocument::new()));
+    let mut handles = vec![];
+
+    for j in 1..10 {
+        let mutex = Arc::clone(&arc);
+        let handle = thread::spawn(move || {
+            let mut doc = mutex.lock().expect("Failed to lock");
+            let str = format!("Thread {}", j);
+            doc.add(String::from(str));
+
+            thread::sleep(Duration::from_millis(100));
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    for x in arc.lock().unwrap().iter() {
+        println!("{}", x);
+    }
 }
